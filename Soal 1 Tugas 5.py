5dbf26c3175e47bfe3b3223de374c500d151e6bb
muhammadfagih <80554853+muhammadfagih@users.noreply.github.com>
2021-04-03 11:46:47 +0700

    Add files via upload

---
+++ Soal 1 Tugas 5.py
@@ -0,0 +1,9 @@
+#penggunaan if untuk dua kasus
+Nama = (input('nama :'))
+Jenis_Kelamin = (input("Jenis Kemin (L/P):"))
+#memeriksa
+if Jenis_Kelamin = 'L':
+    print('Tuan', Nama)
+else:
+    print('Nyonya', Nama)
+print()
